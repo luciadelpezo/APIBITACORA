1c91c3e19628d81a953664e06772e49b60d47389
luciadelpezo <luciadelpezoreyes4@gmail.com>
2026-07-19 03:15:34 -0500

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -5,18 +5,22 @@ import vehiculosRoutes from './routes/vehiculos.routes.js';
 import mantenimientosRoutes from './routes/mantenimientos.routes.js';
 import recordatoriosRoutes from './routes/recordatorios.routes.js';
 import reportesRoutes from './routes/reportes.routes.js';
+import path from 'path';
 
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:8100', // Pon el origen exacto en lugar de '*'
-
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'], 
     credentials: true
 };
 
 app.use(cors(corsOptions)); // Habilitar los cors
+
+app.options(/^\/api\/.*/, cors(corsOptions));
 app.use(express.json()); 
+
 // Rutas
 app.use('/api', authRoutes);
 app.use('/api', vehiculosRoutes);
